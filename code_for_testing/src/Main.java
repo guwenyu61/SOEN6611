0e6cfc8aa2d32b738f404d8933cfea3c238f00e2
fish <fishisycho@gmail.com>
2017-03-13 22:51:10 -0400

    [Adding] code for testing

---
+++ code_for_testing/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t = new Test();
+		int aaa = t.aaa;
+		t.output();
+		t.output();
+	}
+
+}
---
+++ code_for_testing/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public int aaa = 0;
+	public int bbb = 1;
+	public Test() {
+		
+	}
+	
+	public void output() {
+		System.out.println("XX");
+	}
+	
+	public void input() {
+		
+	}
+}
